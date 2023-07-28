03ad84d2d795e95b20de171236bd7202991f4279
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-07-28 05:59:10 +0000

    [update]railsにjs,jqueryの実装

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -6,3 +6,20 @@ $(document).ready(function() {
     $(this).css('color','red');
   });
 });
+
+// Swiperのオプションの定数化
+const opt = {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navgation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+}
+
+//  Swiperを実行（初期化）
+$(document).on('turbolinks:load', function() {
+  let swiper = new Swiper('.swiper',opt);
+});
